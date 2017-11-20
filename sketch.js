0eebe4a3b0c26cf2651b4d9ef82a12cd67eb3e4c
David Weiß <davidweisspost@gmail.com>
2017-11-20 22:19:36 +0100

    Introduce modularity with classes (No ES6)

--- sketch.js
+++ sketch.js
@@ -15,45 +15,6 @@ function draw() {
   }
   myComet.show()
   if(myComet.hasCollision()) {
-    noLoop()
+    noLoop() // end of game
   }
-}
-
-class Comet {
-  constructor() {
-    this.x = 200
-    this.y = 100
-    this.tail = []
-  }
-
-  fall() {
-    this.y += gravity
-    gravity *= 1.05
-  }
-
-  rise() {
-    this.y += -5
-    gravity = 2
-  }
-
-  show() {
-    noStroke()
-    fill(250, 250, 10)
-    ellipse(this.x, this.y, 50, 40)
-
-    this.tail.forEach(function(tailItem, tailIndex) {
-      fill(25*((29-tailIndex)), 25*(29-tailIndex), 5*(29-tailIndex))
-      ellipse(200-(20-tailIndex)*10, tailItem, 2*tailIndex, 2*tailIndex)
-    })
-
-    this.tail.push(this.y)
-    if(this.tail.length > 20) {
-      this.tail.shift()
-    }
-  }
-
-  hasCollision() {
-    return this.y+25 > height || this.y < 0
-  }
-
 }
